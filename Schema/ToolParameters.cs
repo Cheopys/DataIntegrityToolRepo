599ac7e3b8196b0005e282e12c4cf0a5620d7893
Chris Fox <cheopys@gmail.com>
2025-04-15 23:38:05 +0700

    tool parameters

--- Schema/ToolParameters.cs
+++ Schema/ToolParameters.cs
@@ -2,6 +2,7 @@
 {
     public class ToolParameters
     {
+        public Int16  Id            { get; set; }
         public byte[] publicKey     { get; set; } 
         public byte[] privateKey    { get; set; }
     }
