52cb90dfd0bedb82e935321d5be11d8d4bac41ca
Chris Fox <cheopys@gmail.com>
2025-05-08 11:52:26 +0700

    test controller

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -19,24 +19,9 @@ namespace DataIntegrityTool.Controllers
 {
 	[ApiController]
 	[Route("[controller]")]
+
 	public class TestController : ControllerBase
 	{
-		static Logger logger;
-		public TestController()
-		{
-			var config = new NLog.Config.LoggingConfiguration();
-
-			// Targets where to log to: File and Console
-			var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
-
-			// Rules for mapping loggers to targets            
-			config.AddRule(NLog.LogLevel.Info, NLog.LogLevel.Fatal, logconsole);
-
-            // Apply config           
-            LogManager.Configuration = config;
-			logger = LogManager.GetCurrentClassLogger();
-		}
-
 		[HttpPut, Route("RegisterCustomer")]
 		public async Task<Int32> RegisterCustomer(RegisterCustomerRequest request)
 		{
@@ -51,13 +36,14 @@ namespace DataIntegrityTool.Controllers
 
 			return JsonSeriaizer.Serialize(customers);
 		}
-        [HttpGet, Route("GetUsers")]
-        [Produces("application/json")]
-        public async Task<string> GetUsers(Int32 CustomerId)
-        {
-            List<Users> users = await UsersService.GetUsers(CustomerId);
+
+		[HttpGet, Route("GetUsers")]
+		[Produces("application/json")]
+		public async Task<string> GetUsers(Int32 CustomerId)
+		{
+			List<Users> users = await UsersService.GetUsers(CustomerId);
 
 			return JsonSerializer.Serialize(users);
-        }
-    }
+		}
+	}
 }
--- Schema/Customers/UserRegistration.cs
+++ Schema/Customers/UserRegistration.cs
@@ -2,8 +2,8 @@
 {
     public class UserRegistration
     {
-        public Int32 CustomerId         { get; set; }
-        public string          Token    { get; set; }
-        public List<ToolTypes> Tools    { get; set; }
+        public Int32           CustomerId   { get; set; }
+        public string          Token        { get; set; }
+        public List<ToolTypes> Tools        { get; set; }
     }
 }
