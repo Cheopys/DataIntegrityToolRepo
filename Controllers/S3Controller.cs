aaef88d3dd609bc67a18a893149c771365d4ca22
Chris Fox <cheopys@gmail.com>
2025-09-24 01:08:16 +0700

    upload

--- Controllers/S3Controller.cs
+++ Controllers/S3Controller.cs
@@ -6,10 +6,19 @@ namespace DataIntegrityTool.Controllers
 {
 	public class S3Controller : Controller
 	{
+		[HttpGet, Route("DownloadTool")]
 		public async Task<byte[]> DownloadTool(OSType		 ostype, 
 											   InterfaceType interfacetype)
 		{
 			return await S3Service.GetTool(ostype, interfacetype);
+		}
+
+		[HttpPut, Route("UploadTool")]
+		public async Task UploadTool(OSType			ostype,
+									 InterfaceType	interfacetype, 
+									 byte[]			tool)
+		{
+			await S3Service.StoreTool(ostype, interfacetype, tool);
 		}
 	}
 }
