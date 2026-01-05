8777028d8ea21832dee4d1fbedb9541b1d15d644
Chris Fox <cheopys@gmail.com>
2026-01-05 22:05:33 +0700

    add subscription

--- Services/CustomersService.cs
+++ Services/CustomersService.cs
@@ -446,20 +446,18 @@ namespace DataIntegrityTool.Services
 
                 if (customer != null)
                 {
-                    if (custsub == null)
+                    custsub = new CustomerSubscriptions()
                     {
-                        custsub = new CustomerSubscriptions()
-                        {
-                            CustomerId = CustomerId,
-                            SubscriptionId = subscriptionId,
-                        };
+                        CustomerId = CustomerId,
+                        SubscriptionId = subscriptionId,
+                    };
 
-						context.CustomerSubscriptions .Add(custsub);
-                    }
+					context.CustomerSubscriptions .Add(custsub);
+
                     // ExpirationDate is null for a subscription that hasn't been used yet
 
                     if (custsub.ExpirationDate == null
-                    && customer.SubscriptionTime != null)
+                    &&  customer.SubscriptionTime != null)
                     {
                         customer.SubscriptionTime += TimeSpan.FromDays(subscription.days);
                     }
