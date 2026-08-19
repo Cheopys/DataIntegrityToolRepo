403911d0e8eb9ccef4b4e67c4e52753767f8c2b4
Chris Fox <cheopys@gmail.com>
2026-08-19 23:01:07 +0700

    prelive

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 
 if (builder.Environment.IsDevelopment())
 {
@@ -72,6 +72,7 @@ WebApplication app = builder.Build();
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
 // Configure the HTTP request pipeline.
+/*
 if (app.Environment.IsDevelopment())
 {
 	app.UseSwagger();
@@ -83,7 +84,6 @@ else
 	app.UseSwaggerUI();
 	//app.UseHttpsRedirection();
 }
-/*
 app.UseForwardedHeaders(new ForwardedHeadersOptions
 {
 	ForwardedHeaders = ForwardedHeaders.XForwardedFor 
--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,7 @@ echo "Building the source..."
 dotnet publish -c Release -o bin/Release/net8.0/
 
 echo "Starting ..."
-dotnet run --configuration Release --urls="http://0.0.0.0:5000;https://0.0.0.0:5001" &
+# dotnet run --configuration Release --urls="http://0.0.0.0:5000;https://0.0.0.0:5001" &
+ASPNETCORE_ENVIRONMENT=Production dotnet bin/Release/net8.0/DataIntegrityTool.dll &
+
 
